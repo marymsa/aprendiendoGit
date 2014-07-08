79c264e17f8e13d11743f56dbf9876f3bdc73894
Marielby Soares <Marielby Soares@desarroll00>
2014-07-08 08:55:52 -0430

    Primer commit! De pom y PruebaGit1

---
+++ src/main/java/PruebaGit1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marielby Soares
+ */
+public class PruebaGit1 {
+    int hola;
+    int chao;
+}
